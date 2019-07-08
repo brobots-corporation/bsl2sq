136b1c062b65c03b721fa8b6016184071756f332
MWixBot <maximkin@mail.ru>
2019-07-08 18:19:18 +0300

    Оптимизация кода и добавление сплиттера для разных ОС

--- src/objects_sonar.py
+++ src/objects_sonar.py
@@ -6,7 +6,7 @@ import xml.dom.minidom as minidom
 import re
 
 # Создание парсера аргументов командной строки
-def createparser():
+def createParser():
     parser = argparse.ArgumentParser()
     parser.add_argument('sourcedirectory', type=str,
                         help='путь к корневой папке с выгруженной конфигурацией 1с')
@@ -16,11 +16,13 @@ def createparser():
                         help='полный путь к файлу, в который будет выполняться выгрузка путей объектов метаданных')
     parser.add_argument('-a', '--absolute', action='store_const', const=True, default=False,
                         help='в случае указания флага будут выгружаться полные пути к файлам. без флага только относительные пути')
+    parser.add_argument('-u', '--unicode', action='store_const', const=True, default=False,
+                        help='в случае указания флага будут выгружаться значение в виде символов Unicode')
 
     return parser
 
 # Процедура проверки аргументов командной строки
-def checkargs(args):
+def checkArgs(args):
     # Проверка существования указанной папки
     if not os.path.exists(args.sourcedirectory):
         print(args.sourcedirectory+" - папка не существует")
@@ -51,39 +53,66 @@ def getObjects(file):
     
     return metadata
 
+# Получение пути к подсистемам в зависимости от указания пути и ОС
+def getPathToFolder(args, folder_name, splitter):
+    if args.sourcedirectory[-1] == splitter:
+        path_to_folder = args.sourcedirectory+folder_name+splitter
+    else:
+        path_to_folder = args.sourcedirectory+splitter+folder_name+splitter
+
+    return path_to_folder
+
+# Получение наименование объектов метаданных в указанных подсистемах
+def getMetadataName(subsystems_path, splitter):
+    os.chdir(subsystems_path)
+    # поиск файлов подсистем с заданным префиксом
+    subsystems_folders = glob.glob("**"+splitter+args.parseprefix+"*.xml", recursive=True)
+
+    set_metadata_name = set()
+    list_metadata_name = []
+
+    for subsystem_folder in subsystems_folders:
+        for metadata_name in getObjects(subsystems_path+subsystem_folder):
+            set_metadata_name.add(metadata_name)
+
+    list_metadata_name = list(set_metadata_name)
+    list_metadata_name.sort()
+
+    return list_metadata_name
+
+# Получение путей к bsl файлам для определенных метаданных
+# def getBslFilesPath(list_metadata_name, source_path, full_path=False):
+#     for metadata_name in list_metadata_name:
+         
+#         os.chdir(source_path+metadata_name)
+
+
+########################################################################################################
+
+
 # Получение парсера и разбор параметров командной строки
-parser = createparser()
+parser = createParser()
 args = parser.parse_args()
 
 # Проверка аргументов командной строки
-checkargs(args)
+checkArgs(args)
 
-SUBSYSTEMS_PATH = ""
+# Определение ОС Windows и сплиттера, для замены слешей в путях к файлам и папкам
+# windows_os = True if sys.platform.find("window") != -1 else False
+splitter = "\\" if sys.platform.find("window") != -1  else "/"
 
-# проверка окончания пути
-if args.sourcedirectory[-1] == "/":
-    SUBSYSTEMS_PATH = args.sourcedirectory+"subsystems/"
-else:
-    SUBSYSTEMS_PATH = args.sourcedirectory+"/subsystems/"
+subsystems_path = getPathToFolder(args, "subsystems", splitter)
 
-# установка текущий директории
-os.chdir(SUBSYSTEMS_PATH)
-# поиск файлов подсистем с заданным префиксом
-subsystems_files = glob.glob("**/"+args.parseprefix+"*.xml", recursive=True)
+# Получение наименование объектов метаданных
+list_metadata_name = getMetadataName(subsystems_path, splitter)
 
-set_metadata = set()
-list_metadata = []
+# set_bsl_files = set()
 
-for file in subsystems_files:
-    # with open(SUBSYSTEMS_PATH+file) as xml_file:
-    #     xml_body = xml_file.read()
-    for obj in getObjects(SUBSYSTEMS_PATH+file):
-        set_metadata.add(obj)
-
-list_metadata = list(set_metadata)
-list_metadata.sort()
+# for metadata in list_metadata:
+#     for obj in getObjects(SUBSYSTEMS_PATH+file):
+#         set_metadata.add(obj)
 
 linenum = 1
-for li in list_metadata:
+for li in list_metadata_name:
     print(str(linenum)+". "+li)
     linenum = linenum + 1

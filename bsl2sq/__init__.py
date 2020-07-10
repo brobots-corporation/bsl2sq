5dcbedc2d80de2a50ecf0e941f9c13b12830ebae
MWixBot <maximkin@mail.ru>
2020-07-10 14:55:17 +0300

    update __init__.py

---
+++ bsl2sq/__init__.py
@@ -0,0 +1,19 @@
+"""
+Поиск по подсистемам в папке конфигурации 1С (разложенной на файлы) bsl файлы с программным кодом.
+Подстановка списка файлов в различных вариациях в файл sonar-project.properties
+"""
+
+
+__author__ = 'Aleksey Maksimkin'
+__maintainer__ = __author__
+
+__email__ = 'maximkin@mail.ru'
+__license__ = 'GPL-3'
+
+
+__all__ = (
+    '__author__',
+    '__email__',
+    '__license__',
+    '__maintainer__'
+)
